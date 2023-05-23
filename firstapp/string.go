f3dd4dffad682b4752c1d01b7e74d1e1ebf5b6d2
aakashdeveloper <ahanda205@gmail.com>
2023-05-24 00:49:56 +0530

    typeconversion

---
+++ firstapp/string.go
@@ -0,0 +1,11 @@
+var a string= `This is string`
+var b string= "Test Data"
+var c = "String Data"
+c := "String Data"
+
+var i int = 32
+var f float32
+
+f = i //error
+f = float32(i)
+
---
+++ firstapp/typeConversion.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main(){
+	var a string
+	a = "foo"
+
+	fmt.Println(a)
+
+	var b int = 99
+	fmt.Println(b)
+
+	d := 3.145 // defaulr float 64
+	fmt.Println(d)
+
+	var e int = int(d)
+	fmt.Println(e)
+
+}
